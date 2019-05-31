c35e4a5b31b508ce05f39c0ffd66515b94a36028
Gilda Snow <gilda.snow@gmail.com>
2019-05-31 00:37:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,9 @@
 def unsafe?(speed)
 
-  if speed > 60 and speed < 40
-    return "Unsafe"
-  else
-    return "Safe"
-  end
 end
 
+
+
 def not_safe?(speed)
-	speed > 60 && speed < 40 ? "Unsafe" : "Safe"
-	end
+	
 end
